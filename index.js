03b9313981d3d5931c54f6d6073dbf6ff4ccb3ab
Scott Taylor <scott@motel.is>
2019-05-28 16:05:51 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Mixins
+export { BehavesAsBelongsTo } from './mixins/BehavesAsBelongsTo'
+export { BehavesAsPanel } from './mixins/BehavesAsPanel'
+export { Deleteable } from './mixins/Deleteable'
+export { Filterable } from './mixins/Filterable'
+export { FormField } from './mixins/FormField'
+export { HandlesValidationErrors } from './mixins/HandlesValidationErrors'
+export { InteractsWithQueryString } from './mixins/InteractsWithQueryString'
+export { InteractsWithResourceInformation} from './mixins/InteractsWithResourceInformation'
+export { Paginatable } from './mixins/Paginatable'
+export { PerformsSearches } from './mixins/PerformsSearches'
+export { PerPageable } from './mixins/PerPageable'
+export { Toggle } from './mixins/Toggle'
+
+// Utils
+export { Capitalize } from './utils/capitalize'
+export { Errors } from './utils/errors'
+export { Minimum } from './utils/minimum'
+export { UsStates } from './utils/us-states'
---
+++ mixins/Deleteable.js
@@ -0,0 +1,59 @@
+export const Deleteable = {
+  methods: {
+    /**
+     * Open the delete menu modal.
+     */
+    openDeleteModal() {
+      this.deleteModalOpen = true;
+    },
+
+    /**
+     * Delete the given resources.
+     */
+    deleteResources(resources, callback = null) {
+      return ExTeal.request({
+        url: `api/${this.resourceName}`,
+        method: "delete",
+        params: {
+          ...this.queryString,
+          ...{ resources: mapResources(resources) }
+        }
+      }).then(
+        callback
+          ? callback
+          : () => {
+              this.deleteModalOpen = false;
+              this.getResources();
+            }
+      );
+    },
+
+    /**
+     * Delete the selected resources.
+     */
+    deleteSelectedResources() {
+      this.deleteResources(this.selectedResources);
+    },
+
+    /**
+     * Delete all of the matching resources.
+     */
+    deleteAllMatchingResources() {
+      return ExTeal.request({
+        url: `api/${this.resourceName}`,
+        method: "delete",
+        params: {
+          ...this.queryString,
+          ...{ resources: "all" }
+        }
+      }).then(() => {
+        this.deleteModalOpen = false;
+        this.getResources();
+      });
+    }
+  }
+};
+
+function mapResources(resources) {
+  return resources.map(resource => resource.id).join(",");
+}
